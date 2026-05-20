a614589bd612da1426920afac7b2b8d95a89388c
Han Jones <120652943+hanalysis@users.noreply.github.com>
2026-05-20 11:31:28 +0100

    multiQC - removing mean_umis

--- modules/local/clipqc/templates/clipqc.py
+++ modules/local/clipqc/templates/clipqc.py
@@ -93,7 +93,7 @@ def main(process_name):
     # ==========
 
     dedup_logs = sorted(["collapse/" + f for f in os.listdir("collapse") if f.endswith(".log")])
-    dedup = dict((key, []) for key in ["exp", "input_reads", "output_reads", "mean_umis", "ratio"])
+    dedup = dict((key, []) for key in ["exp", "input_reads", "output_reads", "ratio"])
 
     for dedup_log in dedup_logs:
         with open(dedup_log, "r") as logfile:
@@ -107,13 +107,10 @@ def main(process_name):
             output_reads = [i for i in lines if "Number of reads after deduplicating" in i]
             output_reads = int(re.findall(r"\d+", output_reads[0])[-1])
 
-            mean_umis = [i for i in lines if "Average number of UMIs per alignment position" in i]
-            mean_umis = float(re.findall(r"\d+", mean_umis[0])[-1])
 
             dedup["exp"].append(exp)
             dedup["input_reads"].append(input_reads)
             dedup["output_reads"].append(output_reads)
-            dedup["mean_umis"].append(mean_umis)
             dedup["ratio"].append(round(input_reads / output_reads, 2))
 
     dedup_df = pd.DataFrame(dedup)
@@ -121,7 +118,6 @@ def main(process_name):
 
     # Subset for MultiQC plots
     dedup_df.loc[:, ["exp", "input_reads", "output_reads"]].to_csv("dedup_reads.tsv", sep="\t", index=False)
-    dedup_df.loc[:, ["exp", "mean_umis"]].to_csv("dedup_mean_umis.tsv", sep="\t", index=False)
     dedup_df.loc[:, ["exp", "ratio"]].to_csv("dedup_ratio.tsv", sep="\t", index=False)
 
     print(dedup_df)
