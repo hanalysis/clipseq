f84bba22ac1f5d55df22b687e9dffb221e163cb5
Han Jones <120652943+hanalysis@users.noreply.github.com>
2026-09-03 23:12:40 +0100

    also adding source prepending

--- modules/local/multimap_class_binning/templates/multimap_class_binning.py
+++ modules/local/multimap_class_binning/templates/multimap_class_binning.py
@@ -134,6 +134,7 @@ def gtf_to_category_bed(gtf_path):
             else:
                 cat = cols[int(COL)-1]
                 cats_list.append(cat)
+                cat = source + ": " + cat
                 cat = cat.replace(" ", "_")
                 fout.write(f"{chrom}\t{start}\t{end}\t{cat}\t0\t{strand}\n")
                 n += 1
